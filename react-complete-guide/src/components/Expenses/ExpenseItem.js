1e59f5efe72ba2b60470bbc191838f068dd7899e
Sharukhan-N <sharukhannofficial@gmail.com>
2022-07-19 11:21:13 +0530

    useState working

--- react-complete-guide/src/components/Expenses/ExpenseItem.js
+++ react-complete-guide/src/components/Expenses/ExpenseItem.js
@@ -1,14 +1,24 @@
 import "./ExpenseItem.css";
+import React, { useState } from "react";
 import ExpenseAmount from "./ExpenseAmount";
 import Card from "../UI/Card";
 
 const ExpenseItem = (props) => {
+
+
+    const [title, setTitle] = useState(props.title);
+
+    const clickHandler = () => {
+        setTitle("Updated");
+    }
+
+
     return (
         <Card className="card_expense_item">
             <div className="card__body">
                 <span className="tag tag-blue">{props.tag}</span>
-                <h3>{props.date.toDateString()}</h3>
-                <h4>{props.title}</h4>
+                <h3>{props.date.toDateString()}</h3> <span className="tag_end tag-black" onClick={clickHandler}>Edit</span>
+                <h4>{title}</h4>
                 <ExpenseAmount amount={props.amount}></ExpenseAmount>
             </div>
         </Card>
